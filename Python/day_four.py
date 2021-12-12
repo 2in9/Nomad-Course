fa46ce065be01a95c7333ca5eeaa1c54608a8418
이현수 <dldlsrn231@gmail.com>
2021-12-12 15:09:14 +0900

    Update day_four.py

--- Python/day_four.py
+++ Python/day_four.py
@@ -1,19 +1,36 @@
+# 보일러플레이트를 이용하여 URL을 입력받아 온라인 상태인지 아닌지 체크하는 프로그램
+import os
 import requests
-from bs4 import BeautifulSoup
 
-indeed_r = requests.get(
-    "https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit=50")
-indeed_s = BeautifulSoup(indeed_r.text, 'html.parser')
+while True:
+    os.system('cls')
+    print("Welcome to IsItDown.py!\nPlease write a URL or URLs you want to check. (separated by comma)")
 
-pagination = indeed_s.find("ul", {"class": "pagination-list"})
+    url_input = input()
+    url_list = url_input.split(',')
 
-pages = pagination.find_all('a')
+    for url in url_list:
+        url = url.strip()
+        if not"." in url:
+            print(f"{url} is not a valid URL.")
+        else:
+            if not("http://" in url or "https://" in url):
+                url = f"http://{url}"
+            try:
+                r = requests.get(url)
+                if r.status_code < 300:
+                    print(f"{url} is up!")
+                else:
+                    print(f"{url} is down!")
+            except:
+                print(f"{url} is down!")
 
-spans = []
-
-for page in pages:
-    spans.append(page.find("span"))
-
-spans = spans[:-1]
-
-print(spans)
+    while True:
+        restart = input("Do you want to start over? y/n ")
+        if restart is "n":
+            print("k. bye!")
+            quit()
+        elif restart is "y":
+            break
+        else:
+            print("That's not a valid answer.")
