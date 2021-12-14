eef905e507002973fd1090bdd537231c5e9e40ff
이현수 <dldlsrn231@gmail.com>
2021-12-15 04:26:00 +0900

    Update

--- JS/momentum/app.js
+++ JS/momentum/app.js
@@ -1,1 +1,15 @@
-alert("hi");
+const player = {
+    name: "LHS",
+    age: 24,
+    fat: true,
+    habby: "game",
+    hello(aaa, bbb){
+        return aaa+bbb
+    }
+};
+player.fat = false;
+player.lastname = "hahaha"
+player.age += 10;
+console.log(player)
+console.log(player.name)
+console.log(player.hello(1, 2))
